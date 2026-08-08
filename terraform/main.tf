965c738c11e92606386bc6f059f912a0150c9292
Kush Arora <143254165+kush1306@users.noreply.github.com>
2026-08-08 18:16:33 +0530

    Update user data script to include docker-compose symlink
    Add symlink for docker-compose in user data script

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,15 +79,16 @@ resource "aws_instance" "microservices_platform_instance" {
     key_name = "mp_key"
     
     user_data = <<-EOF
-       #!/bin/bash
-       apt-get update
-       apt-get install -y docker.io docker-compose-plugin
-      systemctl enable docker
-      systemctl start docker
-      mkdir -p /usr/local/lib/docker/cli-plugins
-      curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
-      -o /usr/local/lib/docker/cli-plugins/docker-compose
-      chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+      #!/bin/bash
+apt-get update
+apt-get install -y docker.io
+systemctl enable docker
+systemctl start docker
+mkdir -p /usr/local/lib/docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+  -o /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+ln -sf /usr/local/lib/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
 EOF
 
     tags = {
