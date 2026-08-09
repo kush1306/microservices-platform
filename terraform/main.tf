c0ec2206fc55ea1d3e748e18b29fed7a2ac8ba98
Kush Arora <143254165+kush1306@users.noreply.github.com>
2026-08-09 15:39:13 +0530

    Add usermod for docker group and create CLI plugins dir
    Added user to docker group and created directory for Docker CLI plugins.

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,6 +86,8 @@ apt-get install -y docker.io curl
 systemctl enable docker
 systemctl start docker
 
+usermod -aG docker ubuntu
+
 mkdir -p /usr/local/lib/docker/cli-plugins
 
 curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
